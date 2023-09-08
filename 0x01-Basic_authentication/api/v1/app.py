cc32fb75fb5560e59977ef435eebb2a950c6a713
phurhard <phurhardeen@gmail.com>
2023-09-08 22:29:56 +0100

    steady

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,24 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+routes = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+auth = getenv("AUTH_TYPE")
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before():
+    if auth is None:
+        pass
+    elif not auth.require_auth(request.path, routes):
+        pass
+    elif auth.authorization_header(request) is None:
+        abort(401)
+    elif auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -38,4 +56,4 @@ def forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=1)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 """ Authentication base"""
 from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class"""
+    def __init__(self):
+        """Initialization"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False is a user does not require auth"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        elif path in excluded_paths or path+'/' in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization Header"""
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Currrent user"""
+        return None
