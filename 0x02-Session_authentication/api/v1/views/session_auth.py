5d6b9ec4e86d94bcefbe45835528d61540c48273
phurhard <phurhardeen@gmail.com>
2023-09-10 11:06:10 +0100

    pep8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,28 +11,23 @@ def login_session():
     mail = request.form.get('email')
     psswd = request.form.get('password')
     if mail is None or len(mail) == 0:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if psswd is None or len(psswd) == 0:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     user = User.search({"email": mail})
     if user is None or len(user) == 0:
         return jsonify(
-                { "error": "no user found for this email" }
+                {"error": "no user found for this email"}
                 ), 404
     elif not user[0].is_valid_password(psswd):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     else:
         from api.v1.app import auth
         session_id = auth.create_session(user[0].id)
         user_obj = user[0].to_json()
         user_json = jsonify(user_obj)
-        print(f'cookie name: {auth.session_cookie_name} and id: {user_obj["id"]}')
         resp = make_response(user_json)
-        resp.set_cookie(auth.session_cookie_name, str(user_obj['id']))
-        cook = request.cookies
-        print(f'cook: {len(cook)}')
-        print(resp)
-        for k,v in cook.items():
-            print(f'key: {k}, value: {v}')
+        resp.set_cookie(auth.session_cookie_name,
+                        str(session_id))
         return user_json
