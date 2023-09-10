185105053474ee1a416ca35ee40442f256b87801
phurhard <phurhardeen@gmail.com>
2023-09-10 08:26:53 +0100

    h

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,7 +31,7 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(
                 session_id, str):
             return None
-        return self.user_id_by_session_id.get(session_id,'nothing')
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """Returns user instance based on cookie value"""
