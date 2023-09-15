ffdefd7186c9839ee20f3afc3834ce1a02259c1f
phurhard <phurhardeen@gmail.com>
2023-09-15 04:25:07 +0100

    flaskusr

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,7 @@
+# User Authetication service
+    Never make a authenticatation system yourself, make use of verified and industry used services, like flask-User.
+
+## Modules/Libraries used
+     sqlalchemy
+     bcrypt
+     typing
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,49 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from typing import List
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str,
+                 hashed_password: str) -> User:
+        '''Creates a new user n adds to the db'''
+        session = self._session
+        newUser = User()
+        newUser.email = email
+        newUser.hashed_password = hashed_password
+        session.add(newUser)
+        session.commit()
+        return newUser
+
+    def find_user_by(param: str) -> List:
+        '''Finds the first occurence of a user'''
+        session = self._session
+        user = session.query(User).filter(param).first()
+        return user
