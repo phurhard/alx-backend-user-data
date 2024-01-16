f3b2efd7f91ab5759fb65f95803a271ab20046c7
phurhard <phurhardeen@gmail.com>
2024-01-16 19:29:19 +0100

    gitignore

--- 0x01-Basic_authentication/ma.py
+++ 0x01-Basic_authentication/ma.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     if res is not None:
         print("user_object_from_credentials must return None if 'user_email' is not linked to any user")
         exit(1)
-    
+
     print("OK", end="")
