0c043935bb5069114741cea5a70470ed6cd9f9bd
phurhard <phurhardeen@gmail.com>
2024-01-18 21:10:52 +0100

    again

---
+++ 0x02-Session_authenticationn/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Session Auth views"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request, make_response
+from models.user import User
+
+
+@app_views.route('/auth_session/login/',
+                 strict_slashes=False, methods=["POST"])
+def login_session():
+    """Authetication for login"""
+    mail = request.form.get('email')
+    psswd = request.form.get('password')
+    if mail is None or len(mail) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if psswd is None or len(psswd) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": mail})
+    if user is None or len(user) == 0:
+        return jsonify(
+                {"error": "no user found for this email"}
+                ), 404
+    elif not user[0].is_valid_password(psswd):
+        return jsonify({"error": "wrong password"}), 401
+    else:
+        from api.v1.app import auth
+        session_id = auth.create_session(user[0].id)
+        user_obj = user[0].to_json()
+        user_json = jsonify(user_obj)
+        resp = make_response(user_json)
+        resp.set_cookie(auth.session_cookie_name,
+                        str(session_id))
+        return user_json
