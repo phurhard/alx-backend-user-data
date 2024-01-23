95596e2ea1f0d87efd67537140359e58bfe4a416
phurhard <phurhardeen@gmail.com>
2024-01-23 11:49:00 +0100

    find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,9 +45,8 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         '''Finds the first occurence of a user'''
-        session = self._session
         try:
-            user = session.query(User).filter_by(**kwargs).first()
+            user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
                 raise NoResultFound
             return user
